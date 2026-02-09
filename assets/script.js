3aedc7e7fb2fdba383ad72fad6298cc907700476
pablitodev1 <pablo.navarro.campos@gmail.com>
2026-02-09 10:43:42 -0300

    actualizacion

--- assets/script.js
+++ assets/script.js
@@ -10,3 +10,34 @@ function closeSidebar(){
     navbar.classList.remove('show')
     overlay.classList.remove('overlay_show')
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+    const botonesAbrir = document.querySelectorAll('.btn_abrir');
+    const botonesCerrar = document.querySelectorAll('.btn_cerrar');
+    const overlays = document.querySelectorAll('.modal_overlay');
+
+    // Abrir modal
+    botonesAbrir.forEach(btn => {
+        btn.addEventListener('click', () => {
+            const modalId = btn.getAttribute('data-target');
+            document.getElementById(modalId).classList.add('active');
+            document.body.style.overflow = 'hidden'; // Evita scroll al fondo
+        });
+    });
+
+    // Cerrar modal al hacer click en el botón (X)
+    botonesCerrar.forEach(btn => {
+        btn.addEventListener('click', () => {
+            btn.closest('.modal_overlay').classList.remove('active');
+            document.body.style.overflow = 'auto'; // Devuelve el scroll
+        });
+    });
+
+    // Cerrar modal al hacer click fuera de la caja blanca
+    window.addEventListener('click', (e) => {
+        if (e.target.classList.contains('modal_overlay')) {
+            e.target.classList.remove('active');
+            document.body.style.overflow = 'auto';
+        }
+    });
+});
